80f9e819103526e1028fe91b81d905a89cd4660a
gurramkrishn <156026884+gurramkrishn@users.noreply.github.com>
2024-01-12 09:35:40 -0600

    Update Department.java
    updated department.java

--- Department.java
+++ Department.java
@@ -1,11 +1,3 @@
 public abstract class Department {
     abstract int getDepartmentSize();
 }
-
-class ComputerScience extends Department {
-    @Override
-    public int getDepartmentSize() {
-        return 50;
-    }
-
-}
